e029bdebff7fb09e3f32a78e08101c94d5bb7252
Chocobone <yooho1030@naver.com>
2025-11-11 00:46:15 +0900

    251111_test

--- AACommu.sh
+++ AACommu.sh
@@ -5,15 +5,15 @@
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=29G
 #SBATCH -p batch_ugrad
-#SBATCH -w aurora-g8
+#SBATCH -w aurora-g7
 #SBATCH -t 1-0
 #SBATCH -o logs/slurm-%A.out
 
 
 /data/yho7374/anaconda3/bin/conda init
 source ~/.bashrc
-conda activate training
+conda activate AACommu
 
-jupyter execute AAC_model.ipynb 
+jupyter nbconvert --to notebook --execute AAC_model.ipynb 
 
 exit 0
